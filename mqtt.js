319ae8009113f78bb14e738f5fc38f4c51783124
Timago <38229624+Timago@users.noreply.github.com>
2018-05-01 20:48:29 +0200

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -1,7 +1,8 @@
+//Verbindungsaufbau zum Broker auf Shiftr.io
 var client = mqtt.connect('wss://7ab8d043:4acf899e28d7740c@broker.shiftr.io:443', { //Wichtig wss und port! Sonst keine sichere Verbindung im Browser
   clientId: 'greenhouse-webapp'
 });
- 
+//Bei erfolgreicher Verbindung Hinweis in der Browser Konsole
 client.on('connect', function(){
    console.log('client has connected!');
 });
@@ -10,7 +11,8 @@ client.on('connect', function(){
 var displayNames=["LightStartTime","LightStopTime","LightLevel","LightSwitch","Temperature","AirHumidity","FanSwitch","SoilHumidity","DryTargetValue","HumidTargetValue","PumpSwitch"]; 
 var displayData=["10 uhr","20 uhr","20 Lumen","Off","20°C","60%","Off","50%","70%","60%","Off"]; 
 
-    
+//Die Funktion abonniert alle im names Array spezifizierten Kanäle und speichert alle über diese Kanäle
+//empfangenen Daten in den korrespondierenden Stellen im data Array und gibt die Daten in die Konsole aus.
 function setallSubscribers(names,data){
  client.on('message', function(topic, message) {
         for(i=0;i<names.length;i++){
@@ -27,7 +29,10 @@ function setallSubscribers(names,data){
     } 
 }
 
+//abboniert alle zur Darstellung relevanten Daten
+setallSubscribers(displayNames,displayData);
 
+//Sendet analog zu setallSubscribers auf den im names Array spezifizierten Kanälen die Daten aus dem data Array.
 function setallPublishers(names,data){    
    setInterval(function(){
     for(i = 0; i < names.length; i++){
