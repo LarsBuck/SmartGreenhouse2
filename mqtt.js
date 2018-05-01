dd53937aa777816e10f75cdd3f62f95cf52091bc
Timago <38229624+Timago@users.noreply.github.com>
2018-05-01 19:06:30 +0200

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -1,7 +1,9 @@
+//Verbindungsaufbau zum Broker auf Shiftr.io
 var client = mqtt.connect('wss://7ab8d043:4acf899e28d7740c@broker.shiftr.io:443', { //Wichtig wss und port! Sonst keine sichere Verbindung im Browser
   clientId: 'greenhouse-webapp'
 });
- 
+
+//Bei erfolgreicher Verbindung Hinweis in der Browser Konsole
 client.on('connect', function(){
    console.log('client has connected!');
 });
@@ -11,12 +13,12 @@ var displayNames=["LightStartTime","LightStopTime","LightLevel","LightSwitch","T
 var displayData=["10 uhr","20 uhr","20 Lumen","Off","20°C","60%","Off","50%","70%","60%","Off"]; 
 
     
+//Die Funktion abonniert alle im names Array spezifizierten Kanäle und speichert alle über diese Kanäle
+//empfangenen Daten in den korrespondierenden Stellen im data Array und gibt die Daten in die Konsole aus.
 function setallSubscribers(names,data){
  client.on('message', function(topic, message) {
         for(i=0;i<names.length;i++){
             if(topic.toString()=="/smartGreenhouse/Sollwerte/"+names[i]){
-            temp=('#tdata'+i).toString();
-            $(temp).text(message.toString());
             data[i]=message.toString();
             }
         }
@@ -27,7 +29,8 @@ function setallSubscribers(names,data){
     } 
 }
 
-
+//Die Funktion sendet jede Sekunde über alle im names Array spezifizierten Kanäle.
+//Gesendet werden hierbei die korrespondierenden Daten im data Array.
 function setallPublishers(names,data){    
    setInterval(function(){
     for(i = 0; i < names.length; i++){
@@ -36,22 +39,3 @@ function setallPublishers(names,data){
   }, 1000);
 }
 
-
-
-
-
-/*
-function setSubscriber(name){
- client.on('message', function(topic, message) {
-        $('#data3').text(message.toString());
- });
-      client.subscribe('/smartGreenhouse/Sollwerte/Tabelle');
-}
-
-function setPublisher(name,data){
- setInterval(function(){
-        data[i]=data[i].split('').join(',')  //Wichtig Payload der Funktion nur als charArray, daher den String vorher zerteilen !
-        client.publish('/nodemcu/Sollwerte/'+names, data);
-  }, 1000);
-}
-*/
